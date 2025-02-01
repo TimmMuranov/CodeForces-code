92d9517c11a590e75221fc997e246b4fd097dde5
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2025-02-01 11:47:06 +0000

    начал 2060с

--- 2060C.cpp
+++ 2060C.cpp
@@ -2,4 +2,12 @@
 using namespace std;
 
 int main() {
+    int data_size, n, k;
+    cin >> data_size;
+
+    for(int x=0; x<data_size; ++x){
+        cin >> n;
+        cin >> k;
+        //потом решу...
+    }
 }
